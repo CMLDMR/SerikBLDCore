5e8bfd4d3c37e3b1264146eda4d3f02ab0bea191
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-16 12:29:36 +0300

    SERIKBLDCORE_EXPORT Eklendi.

--- user.cpp
+++ user.cpp
@@ -1,6 +1,76 @@
 #include "user.h"
 
-User::User()
+User::User(DB *_db) : Item () , DB(_db)
 {
 
+}
+
+bool User::Login(const std::string &_mTel, const std::string &_mPassword)
+{
+    this->append(this->KeyTel,_mTel);
+    this->append(this->KeyPassword,_mPassword);
+
+    try {
+        auto val = this->db ()->collection (Collection).find_one (this->view ());
+
+        if( val )
+        {
+            this->setDocumentView (val.value ().view ());
+            return true;
+        }
+
+    } catch (mongocxx::exception &e) {
+        std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+        std::cout << str << std::endl;
+    }
+
+    return false;
+}
+
+std::string User::PhotoFilePath()
+{
+    auto element = this->element (KeyFotoid);
+    if( element )
+    {
+        auto url = this->downloadFile (element->get_oid ().value.to_string ().c_str ());
+        return url;
+    }else{
+        return "";
+    }
+}
+
+std::string User::AdSoyad()
+{
+    auto element = this->element (KeyAdSoyad);
+
+    if( element )
+    {
+        return (element->get_utf8 ().value.to_string());
+    }else{
+        return (KeyAdSoyad + " Bilgisi Eksik");
+    }
+}
+
+std::string User::Statu()
+{
+    auto element = this->element (KeyStatu);
+
+    if( element )
+    {
+        return  (element->get_utf8 ().value.to_string());
+    }else{
+        return (KeyStatu + " Bilgisi Eksik");
+    }
+}
+
+std::string User::Birimi()
+{
+    auto element = this->element (KeyBirimi);
+
+    if( element )
+    {
+        return  (element->get_utf8 ().value.to_string());
+    }else{
+        return (KeyBirimi + " Bilgisi Eksik");
+    }
 }
--- user.h
+++ user.h
@@ -1,11 +1,27 @@
 #ifndef USER_H
 #define USER_H
 
+#include "item.h"
+#include "db.h"
 
-class User : public Item
+class SERIKBLDCORE_EXPORT User : public Item , public DB
 {
+    const std::string Collection{"Personel"};
+    const std::string KeyTel = "telefon";
+    const std::string KeyPassword = "password";
+    const std::string KeyAdSoyad{"ad soyad"};
+    const std::string KeyStatu{"Statü"};
+    const std::string KeyBirimi{"Birimi"};
+    const std::string KeyFotoid{"fotooid"};
 public:
-    User();
+    User(DB* _db);
+
+    bool Login( const std::string &_mTel , const std::string &_mPassword);
+
+    std::string PhotoFilePath();
+    std::string AdSoyad();
+    std::string Statu();
+    std::string Birimi();
 };
 
 #endif // USER_H
