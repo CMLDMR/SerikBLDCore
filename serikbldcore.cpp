a6617107f4a3178b65b66fa2ceaca078e02b19a1
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-16 10:54:07 +0300

    Başlatıldı.

---
+++ serikbldcore.cpp
@@ -0,0 +1,5 @@
+#include "serikbldcore.h"
+
+SerikBLDCore::SerikBLDCore()
+{
+}
---
+++ serikbldcore.h
@@ -0,0 +1,12 @@
+#ifndef SERIKBLDCORE_H
+#define SERIKBLDCORE_H
+
+#include "SerikBLDCore_global.h"
+
+class SERIKBLDCORE_EXPORT SerikBLDCore
+{
+public:
+    SerikBLDCore();
+};
+
+#endif // SERIKBLDCORE_H
