f4b0205ae22e992505d38fdd71ff9d15032ab072
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-17 09:10:28 +0300

    Kısmı Düzenleme Yapıldı.

--- dilekce.cpp
+++ dilekce.cpp
@@ -1,6 +1,77 @@
 #include "dilekce.h"
 
-Dilekce::Dilekce()
+
+ const std::string Dilekce::Collection = "Dilekce";
+
+Dilekce::Dilekce(DB *_db) : Item() , DB(_db)
 {
 
+}
+
+boost::optional<Dilekce *> Dilekce::Create_Dilekce(DB *_db)
+{
+    auto item = new Dilekce(_db);
+
+    try {
+        auto ins = _db->db ()->collection (Collection).insert_one (item->view ());
+        if( ins )
+        {
+            item->append("_id",ins.value ().inserted_id ().get_oid ().value );
+            return item;
+        }else{
+            return boost::none;
+        }
+    } catch (mongocxx::exception &e) {
+        std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+        std::cout << str << std::endl;
+        return boost::none;
+    }
+}
+
+void Dilekce::SetSayi(const int &sayi)
+{
+    this->append(KeySayi,sayi);
+}
+
+bool Dilekce::Update()
+{
+
+    auto filter = document{};
+    auto oid = this->element ("_id");
+
+    if( oid )
+    {
+        try {
+            filter.append (kvp("_id",oid->get_oid ()));
+        } catch (bsoncxx::exception &e) {
+            std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+            std::cout << str << std::endl;
+            return false;
+        }
+
+        auto setDoc = document{};
+
+        try {
+            setDoc.append (kvp("$set",this->view ()));
+        } catch (bsoncxx::exception &e) {
+            std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+            std::cout << str << std::endl;
+        }
+
+        auto upt = this->db ()->collection (Collection).update_one (filter.view (),setDoc.view ());
+
+        if( upt )
+        {
+            if( upt.value ().modified_count () )
+            {
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }else{
+        return false;
+    }
 }
--- dilekce.h
+++ dilekce.h
@@ -1,11 +1,32 @@
 #ifndef DILEKCE_H
 #define DILEKCE_H
 
+#include <QtGlobal>
+#include "item.h"
+#include "db.h"
 
-class Dilekce : public Item
+#include "SerikBLDCore_global.h"
+
+
+
+class SERIKBLDCORE_EXPORT Dilekce : public Item , public DB
 {
+    static const std::string Collection;
+    const std::string KeySayi{"Sayı"};
+    const std::string KeyKonu{"Konu"};
+    const std::string KeyIcerik{"Icerik"};
+    const std::string KeyTarihJulian{"TarihJulian"};
+    const std::string KeySaat{"Saat"};
+    const std::string KeyTCOid{"TCOid"};
+    const std::string KeyBirim{"Birim"};
 public:
-    Dilekce();
+    explicit Dilekce(DB* _db);
+
+    static boost::optional<Dilekce*> Create_Dilekce(DB* _db);
+
+    void SetSayi( const int &sayi );
+
+    bool Update();
 };
 
 #endif // DILEKCE_H
