95c140cd8e00b382e6e0e9713e41564db02ea788
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-17 09:09:28 +0300

    Dilekce Sınıfı Eklendi.

---
+++ dilekce.cpp
@@ -0,0 +1,6 @@
+#include "dilekce.h"
+
+Dilekce::Dilekce()
+{
+
+}
---
+++ dilekce.h
@@ -0,0 +1,11 @@
+#ifndef DILEKCE_H
+#define DILEKCE_H
+
+
+class Dilekce : public Item
+{
+public:
+    Dilekce();
+};
+
+#endif // DILEKCE_H
