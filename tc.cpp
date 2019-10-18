62c534cb94b9dfc71448938777040aa00d739fb7
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-18 10:37:46 +0300

    TC Sınıfı Tamamlandı.

--- tc.cpp
+++ tc.cpp
@@ -1,6 +1,65 @@
 #include "tc.h"
+#include <QRandomGenerator>
 
-TC::TC()
+const std::string TC::Collection = "TC";
+
+
+TC::TC(TC *other) : Item() , DB()
 {
+
+    if( other != nullptr )
+    {
+        this->setDocumentView (other->view ());
+    }
+}
+
+void TC::setTCNO(const QString &tcno)
+{
+    this->append(KeyTC,tcno.toStdString ());
+}
+
+void TC::setAdSoyad(const QString &adsoyad)
+{
+    this->append(KeyAdSoyad,adsoyad.toStdString ());
+}
+
+void TC::setCepTelefonu(const QString &cepTelefonu)
+{
+    this->append(KeyCepTelefonu,cepTelefonu.toStdString ());
+
+}
+
+void TC::setMahalle(const QString &mahalle)
+{
+    this->append(KeyMahalle,mahalle.toStdString ());
+
+}
+
+void TC::setTamAdress(const QString &adres)
+{
+    this->append(KeyAdres,adres.toStdString ());
+
+}
+
+void TC::setPassword(const QString &password)
+{
+    if( password == "" )
+    {
+        auto _password = QString::number (1234+(QRandomGenerator::global ()->generate ()%10000));
+        this->append(KeyPassword,_password.toStdString ());
+    }else{
+        this->append(KeyPassword,password.toStdString ());
+    }
+
+}
+
+void TC::setNormalTelefon(const QString &normalTelefon)
+{
+    this->append(KeyNormalTelefon,normalTelefon.toStdString ());
+}
+
+void TC::setCalismaSMS(const bool &smsgitsin)
+{
+    this->append(KeyCalismaSMS,smsgitsin);
 
 }
--- tc.h
+++ tc.h
@@ -1,11 +1,34 @@
 #ifndef TC_H
 #define TC_H
 
+#include <QtGlobal>
+#include "item.h"
+#include "db.h"
+#include <QString>
+#include "SerikBLDCore_global.h"
 
-class TC : public Item
+class TC : public Item , public DB
 {
+    static const std::string Collection;
+    const std::string KeyTC{"TCNO"};
+    const std::string KeyAdSoyad{"İsimSoyisim"};
+    const std::string KeyCepTelefonu{"Cep Telefonu"};
+    const std::string KeyNormalTelefon{"Normal Telefonu"};
+    const std::string KeyMahalle{"Mahalle"};
+    const std::string KeyAdres{"Tam Adres"};
+    const std::string KeyPassword{"password"};
+    const std::string KeyCalismaSMS{"CalismaSMS"};
 public:
-    TC();
+    TC(TC* other = nullptr );
+
+    void setTCNO( const QString &tcno );
+    void setAdSoyad( const QString &adsoyad );
+    void setCepTelefonu( const QString &cepTelefonu );
+    void setMahalle( const QString &mahalle );
+    void setTamAdress( const QString &adres );
+    void setPassword( const QString &password = QString("") );
+    void setNormalTelefon( const QString &normalTelefon );
+    void setCalismaSMS( const bool &smsgitsin = false );
 };
 
 #endif // TC_H
