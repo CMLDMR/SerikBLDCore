c3eef8c628b33b8fda4d8b0c540ae8e2cd3b1601
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-22 15:38:29 +0300

    Create_Dilekce Update ve insertDilekce Methodları Eklendi.

--- dilekcemanager.cpp
+++ dilekcemanager.cpp
@@ -1,6 +1,144 @@
 #include "dilekcemanager.h"
+#include "tc.h"
 
-DilekceManager::DilekceManager()
+
+DilekceManager::DilekceManager() : DB()
 {
 
+}
+
+
+
+boost::optional<Dilekce *> DilekceManager::Create_Dilekce()
+{
+        auto item = new Dilekce();
+
+        try {
+            auto ins = this->db ()->collection (Dilekce::Collection).insert_one (item->view ());
+            if( ins )
+            {
+                item->append("_id",ins.value ().inserted_id ().get_oid ().value );
+                return item;
+            }else{
+                return boost::none;
+            }
+        } catch (mongocxx::exception &e) {
+            std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+            std::cout << str << std::endl;
+            return boost::none;
+        }
+}
+
+bool DilekceManager::Update(Dilekce *dilekce)
+{
+        auto filter = document{};
+        auto oid = dilekce->element ("_id");
+
+        if( oid )
+        {
+            try {
+                filter.append (kvp("_id",oid->get_oid ()));
+            } catch (bsoncxx::exception &e) {
+                std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+                std::cout << str << std::endl;
+                return false;
+            }
+
+            auto setDoc = document{};
+
+            try {
+                setDoc.append (kvp("$set",dilekce->view ()));
+            } catch (bsoncxx::exception &e) {
+                std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+                std::cout << str << std::endl;
+            }
+
+            auto upt = this->db ()->collection (Dilekce::Collection).update_one (filter.view (),setDoc.view ());
+
+            if( upt )
+            {
+                if( upt.value ().modified_count () )
+                {
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+}
+
+bool DilekceManager::insertDilekce(const Dilekce *dilekce)
+{
+
+    try {
+        auto ins = this->insertItem (*dilekce);
+        if( ins )
+        {
+            return true;
+        }
+    } catch (mongocxx::exception &e) {
+        std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+        std::cout << str << std::endl;
+        return false;
+    }
+
+}
+
+QVector<Dilekce> DilekceManager::findDilekce(const Item &itemFilter)
+{
+    QVector<Dilekce> list;
+    auto cursor = this->find (itemFilter);
+    if( cursor )
+    {
+        for( auto doc : cursor.get () )
+        {
+            Dilekce _dilekce;
+            _dilekce.setDocumentView (doc);
+            list.append (_dilekce);
+        }
+    }
+    return list;
+}
+
+QVector<Dilekce> DilekceManager::findByTelefon(const QString &mTelefonNumarasi)
+{
+    QVector<Dilekce> list;
+    std::string TCOid;
+
+    {
+        TC item;
+        item.setCepTelefonu (mTelefonNumarasi);
+
+        auto val = this->findOneItem (item);
+
+        if( val )
+        {
+            item.setDocumentView (val.value ().view ());
+            TCOid = item.oid ().value ().to_string ();
+        }else{
+            return list;
+        }
+
+    }
+
+
+
+    Dilekce item;
+    item.SetTCOid (TCOid.c_str ());
+
+    auto cursor = this->find (item);
+    if( cursor )
+    {
+        for( auto doc : cursor.get () )
+        {
+            Dilekce _dilekce;
+            _dilekce.setDocumentView (doc);
+            list.append (_dilekce);
+        }
+    }
+    return list;
 }
--- dilekcemanager.h
+++ dilekcemanager.h
@@ -1,11 +1,30 @@
 #ifndef DILEKCEMANAGER_H
 #define DILEKCEMANAGER_H
+#include <QtGlobal>
+#include "item.h"
+#include "db.h"
+#include "dilekce.h"
+#include <QString>
+#include "SerikBLDCore_global.h"
+#include <QVector>
 
-
-class DilekceManager : public DB
+class SERIKBLDCORE_EXPORT DilekceManager : public DB
 {
 public:
-    DilekceManager();
+    explicit DilekceManager();
+
+
+    boost::optional<Dilekce*> Create_Dilekce();
+
+    bool Update( Dilekce* dilekce );
+
+    bool insertDilekce( const Dilekce* dilekce );
+
+    QVector<Dilekce> findDilekce(const Item &itemFilter );
+
+    QVector<Dilekce> findByTelefon(const QString &mTelefonNumarasi );
+
+
 };
 
 #endif // DILEKCEMANAGER_H
