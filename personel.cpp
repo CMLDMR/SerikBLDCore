de067f54ffc761da575d7f821370f07f7936d56d
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-25 15:50:33 +0300

    Gerekli Bazı Keyler ve Metodlar eklendi.

--- personel.cpp
+++ personel.cpp
@@ -1,6 +1,97 @@
 #include "personel.h"
 
-Personel::Personel()
+
+const std::string Personel::Collection{"Personel"};
+
+
+
+Personel::Personel() : Item(Collection)
 {
 
+}
+
+Personel::Personel(const Personel &other) : Item(Collection)
+{
+    this->setDocumentView (other.view ());
+}
+
+Personel::Personel(Personel &&other) : Item(Collection)
+{
+    this->setDocumentView (other.view ());
+}
+
+Personel &Personel::operator=(const Personel &other)
+{
+    this->setDocumentView (other.view ());
+    return *this;
+}
+
+Personel &Personel::operator=(Personel &&other)
+{
+    this->setDocumentView (other.view ());
+    return *this;
+}
+
+QString Personel::AdSoyad() const
+{
+    auto val = this->element (KeyAdSoyad);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_utf8 ().value.to_string());
+    }else{
+        return "";
+    }
+}
+
+bool Personel::BuroPersoneli() const
+{
+    auto val = this->element (KeyBuroPersonel);
+    if( val )
+    {
+        return  val.value ().get_bool ().value;
+    }else{
+        return false;
+    }
+}
+
+QString Personel::FotoOid() const
+{
+    auto val = this->element (KeyFotoOid);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_oid ().value.to_string());
+    }else{
+        return "";
+    }
+}
+
+QString Personel::Birim() const
+{
+    auto val = this->element (KeyBirimi);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_utf8 ().value.to_string());
+    }else{
+        return "";
+    }
+}
+
+void Personel::setAdSoyad(const QString &adsoyad)
+{
+    this->append(KeyAdSoyad,adsoyad.toStdString ());
+}
+
+void Personel::setBuroPersoneli(const bool &buro)
+{
+    this->append(KeyBuroPersonel,buro);
+}
+
+void Personel::setFotoOid(const QString &fotoOid)
+{
+    this->append(KeyFotoOid,bsoncxx::oid{fotoOid.toStdString ()});
+}
+
+void Personel::setBirim(const QString &birim)
+{
+    this->append(KeyBirimi,birim.toStdString ());
 }
--- personel.h
+++ personel.h
@@ -2,10 +2,40 @@
 #define PERSONEL_H
 
 
-class Personel : public Item
+#include "item.h"
+#include "SerikBLDCore_global.h"
+
+class SERIKBLDCORE_EXPORT Personel : public Item
 {
+    static const std::string Collection;
+    const std::string KeyAdSoyad{"ad soyad"};
+    const std::string KeyBuroPersonel{"Büro Personeli"};
+    const std::string KeyTelefon{"telefon"};
+    const std::string KeyStatu{"Statü"};
+    const std::string KeyBirimi{"Birimi"};
+    const std::string KeyFotoOid{"fotooid"};
+    const std::string KeyPassword{"password"};
+    const std::string KeyTCNO{"tcno"};
 public:
-    Personel();
+    explicit Personel();
+    Personel( const Personel &other );
+    Personel( Personel &&other );
+
+    Personel& operator=(const Personel &other);
+    Personel& operator=( Personel &&other);
+
+    QString AdSoyad() const;
+    bool BuroPersoneli() const;
+    QString FotoOid() const;
+    QString Birim() const;
+
+
+
+
+    void setAdSoyad( const QString &adsoyad );
+    void setBuroPersoneli( const bool &buro = true );
+    void setFotoOid( const QString &fotoOid );
+    void setBirim( const QString &birim );
 };
 
 #endif // PERSONEL_H
