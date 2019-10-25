1ada8ddd3131a143503b9ed3a57e6080792c13a5
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-25 16:31:25 +0300

    DilekceCevap Eklendi.

---
+++ dilekcecevap.cpp
@@ -0,0 +1,6 @@
+#include "dilekcecevap.h"
+
+DilekceCevap::DilekceCevap()
+{
+
+}
---
+++ dilekcecevap.h
@@ -0,0 +1,11 @@
+#ifndef DILEKCECEVAP_H
+#define DILEKCECEVAP_H
+
+
+class DilekceCevap : public Item
+{
+public:
+    DilekceCevap();
+};
+
+#endif // DILEKCECEVAP_H
