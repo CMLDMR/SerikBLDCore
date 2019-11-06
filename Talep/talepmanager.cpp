0e917e053624cd3c7b1205b087963c35facd7a7e
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-06 16:44:51 +0300

    TalepItem Talep Olarak Değiştirildi.

---
+++ Talep/talepmanager.cpp
@@ -0,0 +1,6 @@
+#include "talepmanager.h"
+
+TalepManager::TalepManager()
+{
+
+}
---
+++ Talep/talepmanager.h
@@ -0,0 +1,11 @@
+#ifndef TALEPMANAGER_H
+#define TALEPMANAGER_H
+
+
+class TalepManager : public DB
+{
+public:
+    TalepManager();
+};
+
+#endif // TALEPMANAGER_H
