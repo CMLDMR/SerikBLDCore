81181c1bf1a31135e2c8c5325269d524c083544e
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-06 16:46:48 +0300

    Yeni constructorlar ve insertTalep Methodu Eklendi.

--- Talep/talepmanager.cpp
+++ Talep/talepmanager.cpp
@@ -1,6 +1,38 @@
 #include "talepmanager.h"
 
-TalepManager::TalepManager()
+TalepManager::TalepManager() : DB()
 {
 
+}
+
+TalepManager::TalepManager(DB *mDB) : DB(mDB)
+{
+
+}
+
+TalepManager::TalepManager(mongocxx::database *_db) : DB(_db)
+{
+
+}
+
+bool TalepManager::insertTalep(const Talep *item)
+{
+    try {
+        auto ins = this->insertItem (*item);
+        if( ins )
+        {
+            if( ins.value ().result ().inserted_count () )
+            {
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    } catch (mongocxx::exception &e) {
+        std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+        std::cout << str << std::endl;
+        return false;
+    }
 }
--- Talep/talepmanager.h
+++ Talep/talepmanager.h
@@ -1,11 +1,24 @@
 #ifndef TALEPMANAGER_H
 #define TALEPMANAGER_H
 
+#include "talep.h"
+#include "db.h"
+#include "Config.h"
 
-class TalepManager : public DB
+
+class SERIKBLDCORE_EXPORT TalepManager : public DB
 {
 public:
     TalepManager();
+    TalepManager(DB* mDB);
+    TalepManager( mongocxx::database* _db );
+
+    bool insertTalep( const Talep* item );
+
+
 };
+
+
+
 
 #endif // TALEPMANAGER_H
