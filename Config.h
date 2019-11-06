ae6df530b107431c7d48ab0397467587ca3b1f44
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-06 09:20:37 +0300

    Platfrom Değişkenleri Eklendi.

--- Config.h
+++ Config.h
@@ -1,4 +1,48 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+
+
+#include <QtGlobal>
+
+
+//#ifdef Q_OS_WIN
+//#define MOBILE
+
+
+
+//#endif
+
+
+//////////////////////////////////
+// CHECK PLATFORM
+#ifdef Q_OS_WIN
+#define DESKTOP
+#endif
+
+
+
+
+
+#ifdef Q_OS_LINUX
+#define DESKTOP
+#endif
+
+#ifdef Q_OS_MACOS
+#define DESKTOP
+#endif
+
+
+#ifdef Q_OS_IOS
+#define MOBILE
+#endif
+
+
+#ifdef Q_OS_ANDROID
+#define MOBILE
+#endif
+//END CHECK PLATFORM
+//////////////////////////////////
+
+
 #endif // CONFIG_H
