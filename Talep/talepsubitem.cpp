9b10096bb232fa3cdbd13c883ba2640b8ee2b84d
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-08 17:28:57 +0300

    Sınıf Tamamlandı.

--- Talep/talepsubitem.cpp
+++ Talep/talepsubitem.cpp
@@ -1,6 +1,196 @@
-#include "talepitem.h"
+#include "talepsubitem.h"
 
-TalepItem::TalepItem()
+
+const std::string TalepSubItem::Collection{"TalepSubItem"};
+const std::string TalepSubItem::Type{"Tip"};
+const std::string TalepSubItem::TalepOid{"talepOid"};
+const std::string TalepSubItem::Aciklama{"Açıklama"};
+const std::string TalepSubItem::Fotograf{"Fotograf"};
+const std::string TalepSubItem::Pdf{"pdf"};
+const std::string TalepSubItem::Konum{"Konum"};
+const std::string TalepSubItem::Video{"Video"};
+const std::string TalepSubItem::Sms{"Sms"};
+
+TalepSubItem::TalepSubItem() : Item (Collection)
 {
+}
 
+TalepSubItem::TalepSubItem(const TalepSubItem &other) : Item (Collection)
+{
+    this->setDocumentView (other.view ());
+}
+
+TalepSubItem::TalepSubItem(TalepSubItem &&other) : Item(Collection)
+{
+    this->setDocumentView (other.view ());
+}
+
+TalepSubItem &TalepSubItem::operator=(const TalepSubItem &other)
+{
+    this->setDocumentView (other.view ());
+    return *this;
+}
+
+TalepSubItem &TalepSubItem::operator=(TalepSubItem &&other)
+{
+    this->setDocumentView (other.view ());
+    return *this;
+}
+
+void TalepSubItem::setType(TalepSubItem::ItemType type)
+{
+    this->append(Type,static_cast<int>(type));
+}
+
+TalepSubItem::ItemType TalepSubItem::type() const
+{
+    auto val = this->element (Type);
+    if( val )
+    {
+        return static_cast<ItemType>(val.value ().get_int32 ().value);
+    }
+    return ItemType::none;
+}
+
+void TalepSubItem::setTalepOid(const QString &talepOid)
+{
+    this->append(TalepOid,bsoncxx::types::b_dbpointer{TalepKey::Collection,bsoncxx::oid{talepOid.toStdString ()}});
+}
+
+QString TalepSubItem::talepOid() const
+{
+    auto val = this->element (TalepOid);
+    if( val )
+    {
+        return QString::fromStdString (val->get_dbpointer ().value.to_string ());
+    }
+    return "";
+}
+
+void TalepSubItem::setAciklama(const QString &aciklama)
+{
+    this->append(Aciklama,aciklama.toStdString ());
+}
+
+QString TalepSubItem::aciklama() const
+{
+    auto val = this->element (Aciklama);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_utf8 ().value.to_string());
+    }
+    return "";
+}
+
+void TalepSubItem::setFotograf(const QString &fotoOid)
+{
+    this->append(Fotograf,bsoncxx::oid{fotoOid.toStdString ()});
+}
+
+QString TalepSubItem::fotografOid() const
+{
+    auto val = this->element (Fotograf);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_oid ().value.to_string ());
+    }
+    return "";
+}
+
+void TalepSubItem::setPdf(const QString &pdfOid)
+{
+    this->append(Pdf,bsoncxx::oid{pdfOid.toStdString ()});
+}
+
+QString TalepSubItem::pdfOid() const
+{
+    auto val = this->element (Pdf);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_utf8 ().value.to_string());
+    }
+    return "";
+}
+
+void TalepSubItem::setKonum(double x, double y)
+{
+    auto doc = document{};
+
+    try {
+        doc.append (kvp("x",x));
+    } catch (bsoncxx::exception &e) {
+        std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+        std::cout << str << std::endl;
+    }
+
+    try {
+        doc.append (kvp("y",y));
+    } catch (bsoncxx::exception &e) {
+        std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+        std::cout << str << std::endl;
+    }
+
+    this->append(Konum,doc);
+}
+
+double TalepSubItem::xCoordinate() const
+{
+    auto val = this->element (Konum);
+    if( val )
+    {
+        try {
+            return val.value ().get_document ().value["x"].get_double ().value;
+        } catch (bsoncxx::exception &e) {
+            std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+            std::cout << str << std::endl;
+            return 0;
+        }
+    }
+    return 0;
+}
+
+double TalepSubItem::yCoordinate() const
+{
+    auto val = this->element (Konum);
+    if( val )
+    {
+        try {
+            return val.value ().get_document ().value["y"].get_double ().value;
+        } catch (bsoncxx::exception &e) {
+            std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+            std::cout << str << std::endl;
+            return 0;
+        }
+    }
+    return 0;
+}
+
+void TalepSubItem::setVideoOid(const QString &videoOid)
+{
+    this->append(Video,bsoncxx::oid{videoOid.toStdString ()});
+}
+
+QString TalepSubItem::videoOid() const
+{
+    auto val = this->element (Video);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_oid ().value.to_string ());
+    }
+    return "";
+}
+
+void TalepSubItem::setSms(const QString &sms)
+{
+    this->append(Sms,sms.toStdString ());
+}
+
+QString TalepSubItem::sms() const
+{
+    auto val = this->element (Sms);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_utf8 ().value.to_string());
+    }
+    return "";
 }
--- Talep/talepsubitem.h
+++ Talep/talepsubitem.h
@@ -1,11 +1,67 @@
-#ifndef TALEPITEM_H
-#define TALEPITEM_H
+#ifndef TALEPSUBITEM_H
+#define TALEPSUBITEM_H
 
+#include "Config.h"
+#include "item.h"
+#include "SerikBLDCore_global.h"
+#include "talep.h"
 
-class TalepItem : public Item
+class SERIKBLDCORE_EXPORT TalepSubItem : public Item
 {
 public:
-    TalepItem();
+
+    enum class ItemType{
+        none = 0,
+        Aciklama,
+        Fotograf,
+        Pdf,
+        Konum,
+        Video,
+        Sms
+    };
+
+    TalepSubItem();
+    TalepSubItem( const TalepSubItem& other );
+    TalepSubItem( TalepSubItem&& other );
+
+    TalepSubItem& operator=( const TalepSubItem& other );
+    TalepSubItem& operator=( TalepSubItem&& other );
+
+    void setType(TalepSubItem::ItemType type);
+    ItemType type() const;
+
+    void setTalepOid( const QString& talepOid );
+    QString talepOid() const;
+
+    void setAciklama( const QString& aciklama );
+    QString aciklama() const;
+
+    void setFotograf( const QString& fotoOid );
+    QString fotografOid() const;
+
+    void setPdf( const QString& pdfOid );
+    QString pdfOid() const;
+
+    void setKonum( double x , double y );
+    double xCoordinate() const;
+    double yCoordinate() const;
+
+    void setVideoOid( const QString& videoOid );
+    QString videoOid() const;
+
+    void setSms( const QString& sms );
+    QString sms() const;
+
+public:
+    static const std::string Collection;
+    static const std::string Type;
+    static const std::string TalepOid;
+    static const std::string Aciklama;
+    static const std::string Fotograf;
+    static const std::string Pdf;
+    static const std::string Konum;
+    static const std::string Video;
+    static const std::string Sms;
 };
 
 #endif // TALEPITEM_H
