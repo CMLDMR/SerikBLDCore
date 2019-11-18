cddec8885144ac7fb62f571bdbeed79496cfa2cb
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-18 16:12:53 +0300

    Dökümantasyon Eklendi.

---
+++ docs/menudata.js
@@ -0,0 +1,34 @@
+/*
+@licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2019 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var menudata={children:[
+{text:"Ana Sayfa",url:"index.html"},
+{text:"İlgili Sayfalar",url:"pages.html"},
+{text:"Sınıflar",url:"annotated.html",children:[
+{text:"Sınıf Listesi",url:"annotated.html"},
+{text:"Sınıf İndeksi",url:"classes.html"},
+{text:"Sınıf Hiyerarşisi",url:"hierarchy.html"},
+{text:"Sınıf Üyeleri",url:"functions.html",children:[
+{text:"Tümü",url:"functions.html"},
+{text:"Fonksiyonlar",url:"functions_func.html"}]}]},
+{text:"Dosyalar",url:"files.html",children:[
+{text:"Dosya Listesi",url:"files.html"}]}]}
