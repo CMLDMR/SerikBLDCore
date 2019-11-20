19fb8f0aefa715712ded298f64efc7407cd8b9ea
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-20 13:49:44 +0300

    SMSAbstractManager Tamamlandı.

--- sms/smsabstrackmanager.cpp
+++ sms/smsabstrackmanager.cpp
@@ -1,6 +1,77 @@
-#include "smsmanager.h"
+#include "smsabstrackmanager.h"
+#include <QDate>
+#include <QTime>
 
-SMSManager::SMSManager()
+
+QString SerikBLDCore::SMSAbstractManager::lastSendedSMSID() const
+{
+    return mLastSendedSMSID;
+}
+
+void SerikBLDCore::SMSAbstractManager::setLastSendedSMSID(const QString &lastSendedSMSID)
+{
+    mLastSendedSMSID = lastSendedSMSID;
+}
+
+SerikBLDCore::SMSAbstractManager::SMSAbstractManager(DB *_db) : DB(_db)
 {
 
+}
+
+bool SerikBLDCore::SMSAbstractManager::canSend(const QString &numara)
+{
+    SMS::SMSItem filter;
+
+    filter.setJulianDay (QDate::currentDate ().toJulianDay ());
+
+    auto val = this->findOneItem (filter);
+
+    if( val )
+    {
+        filter.setDocumentView (val.value ().view ());
+
+        if( filter.secStartOfDay () + 300 * 1000 > QTime::currentTime ().msecsSinceStartOfDay ()  )
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+QVector<SerikBLDCore::SMS::SMSItem> SerikBLDCore::SMSAbstractManager::listSMS(const QString &numara)
+{
+    SMS::SMSItem filter;
+    filter.setNumara (numara);
+
+    QVector<SMS::SMSItem> list;
+
+    auto cursor = this->find (filter);
+
+    if( cursor )
+    {
+        for( auto item : cursor.value () )
+        {
+            SMS::SMSItem smsItem;
+            smsItem.setDocumentView (item);
+            list.push_back (std::move(smsItem));
+        }
+    }
+    return list;
+}
+
+bool SerikBLDCore::SMSAbstractManager::updateSMS(const SerikBLDCore::SMS::SMSItem &item)
+{
+    auto ins = this->updateItem (item);
+    if( ins )
+    {
+        if( ins.value ().modified_count () )
+        {
+            return true;
+        }else{
+            return false;
+        }
+    }else{
+        return false;
+    }
 }
--- sms/smsabstrackmanager.h
+++ sms/smsabstrackmanager.h
@@ -1,11 +1,69 @@
-#ifndef SMSMANAGER_H
-#define SMSMANAGER_H
+#ifndef SMSABSTRACTMANAGER_H
+#define SMSABSTRACTMANAGER_H
+
+#include "SerikBLDCore_global.h"
+#include "db.h"
+#include "smsitem.h"
 
 
-class SMSManager : public DB
+namespace SerikBLDCore {
+
+///
+/// \brief The SMSAbstractManager class
+/// SMS Gönderimi , SMS item Güncelleme ve Gönderilen SMSleri Listeleme Yönetimi Sınıfı
+class SERIKBLDCORE_EXPORT SMSAbstractManager : private DB
 {
+    QString mLastSendedSMSID;
 public:
-    SMSManager();
+    SMSAbstractManager( DB* _db );
+
+
+    ///
+    /// \brief canSend: Verilen Numaraya SMS en az 5 Dakika Ara ile Gönderilmesi Gerekir.
+    /// Eğer Son 5 Dakika için de Sms Gönderilmişse Bu Numaraya SMS Göndermez
+    /// \param numara
+    /// \return bool
+    ///
+    bool canSend( const QString& numara );
+
+    ///
+    /// \brief listSMS : Verilen Numaraya Gönderilen SMSleri listeler
+    /// \param numara
+    /// \return
+    ///
+    QVector<SMS::SMSItem> listSMS( const QString& numara );
+
+    ///
+    /// \brief insertAndSendSMS : SMS Gönderir ve Sistemin Geri Döndürdüğü ID SMS Saklar
+    /// \param item
+    /// \return ID
+    ///
+    virtual SMS::SMSItem& insertAndSendSMS( const SMS::SMSItem& item ) = 0;
+
+    ///
+    /// \brief updateSMS Mevcut SMS Günceller.
+    /// \param item
+    /// \return
+    ///
+    bool updateSMS( const SMS::SMSItem& item );
+
+
+
+    ///
+    /// \brief lastSendedSMSID: Son Gönderilen SMS ID'sini Döndürür. Döndürtükten sonra siler.
+    /// \return
+    ///
+    QString lastSendedSMSID() const;
+
+    ///
+    /// \brief setLastSendedSMSID: Son Gönderilen SMS SetEder.
+    /// \param lastSendedSMSID
+    ///
+    void setLastSendedSMSID(const QString &lastSendedSMSID);
 };
+}
+
+
+
 
 #endif // SMSMANAGER_H
