1a99cf8e41bed5e86c648f0f33e2bec7e3348aed
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-22 15:59:59 +0300

    BilgiEdinmeItem Tamamlandı.

--- bilgiEdinme/bilgiedinmeitem.cpp
+++ bilgiEdinme/bilgiedinmeitem.cpp
@@ -1,6 +1,184 @@
-#include "bilgiedinmeitem.h"
+﻿#include "bilgiedinmeitem.h"
+#include <QDateTime>
 
-BilgiEdinmeItem::BilgiEdinmeItem()
+
+const std::string SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::Collection{"BilgiEdinme"};
+
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::BilgiEdinmeItem(SerikBLDCore::BilgiEdinme::BilgiEdinmeItem *other) : SerikBLDCore::Item (Collection)
 {
+    if( other != nullptr )
+    {
+        this->setDocumentView (other->view ());
+    }else{
 
+    }
+}
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::BilgiEdinmeItem(const SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &other): SerikBLDCore::Item (Collection)
+{
+    this->setDocumentView (other.view ());
+}
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::BilgiEdinmeItem(SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &&other): SerikBLDCore::Item (Collection)
+{
+    this->setDocumentView (other.view ());
+}
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::operator=(const SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &other)
+{
+    this->setDocumentView (other.view ());
+    return *this;
+}
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::operator=(SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &&other)
+{
+    this->setDocumentView (other.view ());
+    return *this;
+}
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::setGeriDonus(const bool &geriDonus)
+{
+    this->append(Key::GeriDonus,geriDonus);
+    return *this;
+}
+
+bool SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::geriDonus() const
+{
+    auto val = this->element (Key::GeriDonus);
+    if( val )
+    {
+        return val.value ().get_bool ().value;
+    }
+    return false;
+}
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::setJulianDay(const int &julianDay)
+{
+    this->append(Key::JulianDay,julianDay);
+    return *this;
+}
+
+int SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::julianDay() const
+{
+    auto val = this->element (Key::JulianDay);
+    if( val )
+    {
+        return val.value ().get_int32 ().value;
+    }
+    return 0;
+}
+
+QString SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::tarih() const
+{
+    auto _oid = this->oid ();
+    if( _oid )
+    {
+        QDateTime::fromTime_t (this->oid ().value ().get_time_t ()).date ().toString ("dd/MM/yyyy");
+    }else{
+        return "01/01/1970";
+    }
+}
+
+QString SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::saat() const
+{
+    auto _oid = this->oid ();
+    if( _oid )
+    {
+        QDateTime::fromTime_t (this->oid ().value ().get_time_t ()).time ().toString ("hh:mm");
+    }else{
+        return "00:00";
+    }
+}
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::setTCoid(const QString &tcoid)
+{
+    this->append(Key::TCOid,bsoncxx::oid{tcoid.toStdString ()});
+    return *this;
+}
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::setTCoid(const bsoncxx::oid &tcoid)
+{
+    this->append(Key::TCOid,tcoid);
+    return *this;
+}
+
+QString SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::tcOid() const
+{
+    auto val = this->element (Key::TCOid);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_oid ().value.to_string ());
+    }
+    return "";
+}
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::setKonu(const QString &konu)
+{
+    this->append(Key::Konu,konu.toStdString ());
+    return *this;
+}
+
+QString SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::konu() const
+{
+    auto val = this->element (Key::Konu);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_utf8 ().value.to_string ());
+    }
+    return "";
+}
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::setMesaj(const QString &mesaj)
+{
+    this->append(Key::Mesaj,mesaj.toStdString ());
+    return *this;
+}
+
+QString SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::mesaj() const
+{
+    auto val = this->element (Key::Mesaj);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_utf8 ().value.to_string ());
+    }
+    return "";
+}
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::setBirim(const QString &birim)
+{
+    this->append(Key::Birim,birim.toStdString ());
+    return *this;
+}
+
+QString SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::birim() const
+{
+    auto val = this->element (Key::Birim);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_utf8 ().value.to_string ());
+    }
+    return "";
+}
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::setCevap(const QString &cevapOid)
+{
+    this->append(Key::CevapOid,bsoncxx::oid{cevapOid.toStdString ()});
+    return *this;
+}
+
+SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::setCevap(const bsoncxx::oid &cevapOid)
+{
+    this->append(Key::CevapOid,cevapOid);
+    return *this;
+}
+
+QString SerikBLDCore::BilgiEdinme::BilgiEdinmeItem::cevapOid() const
+{
+    auto val = this->element (Key::CevapOid);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_oid ().value.to_string ());
+    }
+    return "";
 }
--- bilgiEdinme/bilgiedinmeitem.h
+++ bilgiEdinme/bilgiedinmeitem.h
@@ -1,11 +1,74 @@
 #ifndef BILGIEDINMEITEM_H
 #define BILGIEDINMEITEM_H
 
+#include "Config.h"
+#include "item.h"
+#include "SerikBLDCore_global.h"
 
-class BilgiEdinmeItem : public Item
+
+namespace SerikBLDCore {
+
+
+namespace BilgiEdinme {
+
+namespace Key {
+static const std::string GeriDonus{"geriDonus"};
+static const std::string JulianDay{"julianDay"};
+static const std::string TCOid{"TCOid"};
+static const std::string Konu{"Konu"};
+static const std::string Mesaj{"Mesaj"};
+static const std::string Birim{"Birim"};
+static const std::string CevapOid{"CevapOid"};
+}
+
+
+
+class SERIKBLDCORE_EXPORT BilgiEdinmeItem : public SerikBLDCore::Item
 {
 public:
-    BilgiEdinmeItem();
+    static const std::string Collection;
+
+    BilgiEdinmeItem( BilgiEdinmeItem* other = nullptr );
+    BilgiEdinmeItem( const BilgiEdinmeItem& other );
+    BilgiEdinmeItem( BilgiEdinmeItem&& other );
+
+    BilgiEdinmeItem& operator=( const BilgiEdinmeItem& other );
+    BilgiEdinmeItem& operator=( BilgiEdinmeItem&& other );
+
+    BilgiEdinmeItem& setGeriDonus( const bool& geriDonus );
+    bool geriDonus() const;
+
+    BilgiEdinmeItem& setJulianDay( const int& julianDay );
+    int julianDay() const;
+    QString tarih() const;
+    QString saat() const;
+
+    BilgiEdinmeItem& setTCoid( const QString& tcoid );
+    BilgiEdinmeItem& setTCoid( const bsoncxx::oid &tcoid );
+    QString tcOid() const;
+
+    BilgiEdinmeItem& setKonu( const QString& konu );
+    QString konu() const;
+
+    BilgiEdinmeItem& setMesaj( const QString& mesaj );
+    QString mesaj() const;
+
+    BilgiEdinmeItem& setBirim( const QString& birim );
+    QString birim() const;
+
+    BilgiEdinmeItem& setCevap( const QString& cevapOid );
+    BilgiEdinmeItem& setCevap( const bsoncxx::oid& cevapOid );
+    QString cevapOid() const;
 };
+
+
+
+}
+
+
+
+}
+
+
 
 #endif // BILGIEDINMEITEM_H
