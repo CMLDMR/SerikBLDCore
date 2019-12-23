5225d9f9816403384234b0a829950e5732f9a6a1
Cemal DEMİR <cemaldemir07@gmail.com>
2019-12-23 16:32:55 +0300

    MeclisManager KararManager ve YouTubeManager Tamamlandı.

--- meclis/meclismanager.cpp
+++ meclis/meclismanager.cpp
@@ -1,6 +1,35 @@
 #include "meclismanager.h"
 
-MeclisManager::MeclisManager()
+
+SerikBLDCore::Meclis::MeclisManager::MeclisManager(SerikBLDCore::DB *db)
+    : SerikBLDCore::ListItem<SerikBLDCore::Meclis::MeclisItem> (db)
+{
+
+}
+
+void SerikBLDCore::Meclis::MeclisManager::onList(const QVector<SerikBLDCore::Meclis::MeclisItem> *mlist)
+{
+
+}
+
+SerikBLDCore::Meclis::KararManager::KararManager(SerikBLDCore::DB *db)
+    : SerikBLDCore::ListItem<SerikBLDCore::Meclis::KararItem> (db)
+{
+
+}
+
+void SerikBLDCore::Meclis::KararManager::onList(const QVector<SerikBLDCore::Meclis::KararItem> *mlist)
+{
+
+}
+
+SerikBLDCore::Meclis::YouTubeManager::YouTubeManager(SerikBLDCore::DB *db)
+    : SerikBLDCore::ListItem<SerikBLDCore::Meclis::YouTubeLink> (db)
+{
+
+}
+
+void SerikBLDCore::Meclis::YouTubeManager::onList(const QVector<SerikBLDCore::Meclis::YouTubeLink> *mlist)
 {
 
 }
--- meclis/meclismanager.h
+++ meclis/meclismanager.h
@@ -1,11 +1,59 @@
 #ifndef MECLISMANAGER_H
 #define MECLISMANAGER_H
 
+#include "listitem.h"
+#include "meclisitem.h"
 
-class MeclisManager : public public SerikBLDCore::ListItem<MeclisItem>
+namespace SerikBLDCore {
+
+namespace Meclis {
+
+
+
+/**
+ * @brief The MeclisManager class
+ * Meclis Item, Kararlar ve Canlı Yayın Link Yönetimi
+ */
+class SERIKBLDCORE_EXPORT MeclisManager : public SerikBLDCore::ListItem<SerikBLDCore::Meclis::MeclisItem>
 {
 public:
-    MeclisManager();
+    explicit MeclisManager(DB* db);
+
+
+    virtual void onList(const QVector<MeclisItem> *mlist) override;
+
 };
+
+
+
+class SERIKBLDCORE_EXPORT KararManager : public SerikBLDCore::ListItem<SerikBLDCore::Meclis::KararItem>
+{
+public:
+    explicit KararManager(DB* db);
+
+
+    virtual void onList(const QVector<KararItem> *mlist) override;
+
+};
+
+
+class SERIKBLDCORE_EXPORT YouTubeManager : public SerikBLDCore::ListItem<SerikBLDCore::Meclis::YouTubeLink>
+{
+public:
+    explicit YouTubeManager(DB* db);
+
+
+    virtual void onList(const QVector<YouTubeLink> *mlist) override;
+
+};
+
+
+}
+
+
+
+}
+
+
 
 #endif // MECLISMANAGER_H
