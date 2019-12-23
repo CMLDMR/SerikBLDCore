59fceeff9045f9d0ae01c80a21e50ce100ee9eb3
Cemal DEMİR <cemaldemir07@gmail.com>
2019-12-23 16:32:28 +0300

    MeclisItem Tamamlandı.

--- meclis/meclisitem.cpp
+++ meclis/meclisitem.cpp
@@ -1,6 +1,225 @@
 #include "meclisitem.h"
 
-MeclisItem::MeclisItem()
+
+
+
+
+SerikBLDCore::Meclis::MeclisItem::MeclisItem()
+    :SerikBLDCore::Item(Key::Collection)
 {
 
+}
+
+SerikBLDCore::Meclis::MeclisItem &SerikBLDCore::Meclis::MeclisItem::setYil(const int &yil)
+{
+    this->append(Key::yil,bsoncxx::types::b_int32{yil});
+    return *this;
+}
+
+SerikBLDCore::Meclis::MeclisItem &SerikBLDCore::Meclis::MeclisItem::setAy(const QString &ay)
+{
+    this->append(Key::ay,ay.toStdString ());
+    return *this;
+}
+
+SerikBLDCore::Meclis::MeclisItem &SerikBLDCore::Meclis::MeclisItem::setJulianDay(const int &julianDay)
+{
+    this->append(Key::julianDate,bsoncxx::types::b_int64{julianDay});
+    return *this;
+}
+
+SerikBLDCore::Meclis::MeclisItem &SerikBLDCore::Meclis::MeclisItem::setYayinda(const bool &yayinda)
+{
+    this->append(Key::yayinda,bsoncxx::types::b_bool{yayinda});
+    return *this;
+}
+
+SerikBLDCore::Meclis::MeclisItem &SerikBLDCore::Meclis::MeclisItem::setGundem(const QString &gundem)
+{
+    this->append(Key::gundem,gundem.toStdString ());
+    return *this;
+}
+
+QString SerikBLDCore::Meclis::MeclisItem::gundem() const
+{
+    auto val = this->element (Key::gundem);
+    if( val )
+    {
+        return QString::fromStdString (val->get_utf8 ().value.to_string ());
+    }
+    return "";
+}
+
+QString SerikBLDCore::Meclis::MeclisItem::ay() const
+{
+    auto val = this->element (Key::ay);
+    if( val )
+    {
+        return QString::fromStdString (val->get_utf8 ().value.to_string ());
+    }
+    return "";
+}
+
+int32_t SerikBLDCore::Meclis::MeclisItem::yil() const
+{
+    auto val = this->element (Key::yil);
+    if( val )
+    {
+        return val->get_int32 ().value;
+    }
+    return -1;
+}
+
+std::int64_t SerikBLDCore::Meclis::MeclisItem::julianDay() const
+{
+    auto val = this->element (Key::julianDate);
+    if( val )
+    {
+        return val->get_int64 ().value;
+    }
+    return -1;
+}
+
+bool SerikBLDCore::Meclis::MeclisItem::yayinda() const
+{
+    auto val = this->element (Key::yayinda);
+    if( val )
+    {
+        return val->get_bool ().value;
+    }
+    return false;
+}
+
+
+
+
+
+
+
+SerikBLDCore::Meclis::YouTubeLink::YouTubeLink()
+    :Item(LinkKey::Collection)
+{
+
+}
+
+SerikBLDCore::Meclis::YouTubeLink &SerikBLDCore::Meclis::YouTubeLink::setVideoID(const std::string &videoid)
+{
+    this->append(LinkKey::VideoID,videoid);
+    return *this;
+}
+
+SerikBLDCore::Meclis::YouTubeLink &SerikBLDCore::Meclis::YouTubeLink::setMeclisOid(const std::string &meclisoid)
+{
+    this->append(LinkKey::MeclisOid,bsoncxx::oid{meclisoid});
+    return *this;
+}
+
+std::string SerikBLDCore::Meclis::YouTubeLink::videoid() const
+{
+    auto val = this->element (LinkKey::VideoID);
+    if( val )
+    {
+        return val.value ().get_utf8 ().value.to_string();
+    }
+    return "";
+}
+
+std::string SerikBLDCore::Meclis::YouTubeLink::videoUrl() const
+{
+    auto url = "https://www.youtube.com/watch?v=" + this->videoid ();
+    return url;
+}
+
+std::string SerikBLDCore::Meclis::YouTubeLink::videothump0() const
+{
+    auto url = "https://img.youtube.com/vi/"+this->videoid ()+"/0.jpg";
+    return url;
+}
+
+std::string SerikBLDCore::Meclis::YouTubeLink::videothump1() const
+{
+    auto url = "https://img.youtube.com/vi/"+this->videoid ()+"/1.jpg";
+    return url;
+}
+
+std::string SerikBLDCore::Meclis::YouTubeLink::videothump2() const
+{
+    auto url = "https://img.youtube.com/vi/"+this->videoid ()+"/2.jpg";
+    return url;
+}
+
+std::string SerikBLDCore::Meclis::YouTubeLink::videothump3() const
+{
+    auto url = "https://img.youtube.com/vi/"+this->videoid ()+"/3.jpg";
+    return url;
+}
+
+std::string SerikBLDCore::Meclis::YouTubeLink::meclisoid() const
+{
+    auto val = this->element (LinkKey::MeclisOid);
+    if( val )
+    {
+        return val.value ().get_utf8 ().value.to_string();
+    }
+    return "";
+}
+
+SerikBLDCore::Meclis::KararItem::KararItem()
+    :Item(KararKey::Collection)
+{
+
+}
+
+SerikBLDCore::Meclis::KararItem::KararItem(const std::string &meclisOid)
+    :Item(KararKey::Collection)
+{
+    this->append(KararKey::MeclisOid,bsoncxx::oid{meclisOid});
+}
+
+SerikBLDCore::Meclis::KararItem &SerikBLDCore::Meclis::KararItem::setMeclisOid(const std::string &meclisOid)
+{
+    this->append(KararKey::MeclisOid,bsoncxx::oid{meclisOid});
+    return *this;
+}
+
+SerikBLDCore::Meclis::KararItem &SerikBLDCore::Meclis::KararItem::setKararOid(const std::string &kararOid)
+{
+    this->append(KararKey::KararOid,bsoncxx::oid{kararOid});
+    return *this;
+}
+
+SerikBLDCore::Meclis::KararItem &SerikBLDCore::Meclis::KararItem::setSayi(const int32_t &sayi)
+{
+    this->append(KararKey::Sayi,bsoncxx::types::b_int32{sayi});
+    return *this;
+}
+
+int32_t SerikBLDCore::Meclis::KararItem::sayi() const
+{
+    auto val = this->element (KararKey::Sayi);
+    if( val )
+    {
+        return val.value ().get_int32 ().value;
+    }
+    return 0;
+}
+
+std::string SerikBLDCore::Meclis::KararItem::kararOid() const
+{
+    auto val = this->element (KararKey::KararOid);
+    if( val )
+    {
+        return val.value ().get_oid ().value.to_string ();
+    }
+    return "";
+}
+
+std::string SerikBLDCore::Meclis::KararItem::meclisOid() const
+{
+    auto val = this->element (KararKey::MeclisOid);
+    if( val )
+    {
+        return val.value ().get_oid ().value.to_string ();
+    }
+    return "";
 }
--- meclis/meclisitem.h
+++ meclis/meclisitem.h
@@ -1,11 +1,153 @@
 #ifndef MECLISITEM_H
 #define MECLISITEM_H
 
+#include "item.h"
 
-class MeclisItem : public Item
+namespace SerikBLDCore {
+
+namespace Meclis {
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Meclis Item Ait Keyler
+ */
+namespace Key {
+static const std::string Collection{"MeclisV2"};
+static const std::string ay{"ay"};
+static const std::string yil{"yıl"};
+static const std::string julianDate{"julianDate"};
+static const std::string yayinda{"yayinda"};
+static const std::string gundem{"gundem"};
+}
+
+
+/**
+ * @brief The MeclisItem class
+ * Meclis Kararları ve Canlı Yayın Kayıtlarının Tutulduğu
+ */
+class SERIKBLDCORE_EXPORT MeclisItem : public Item
 {
 public:
-    MeclisItem();
+    explicit MeclisItem();
+
+    MeclisItem& setYil( const int& yil );
+    MeclisItem& setAy( const QString& ay );
+    MeclisItem& setJulianDay( const int& julianDay );
+    MeclisItem& setYayinda( const bool& yayinda );
+    MeclisItem& setGundem( const QString& gundem );
+
+    QString gundem() const;
+    QString ay() const;
+    int32_t yil() const;
+    int64_t julianDay() const;
+    bool yayinda() const;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ *Kararlara Ait Keyler
+ */
+namespace KararKey {
+static const std::string Collection{"MeclisKararlari"};
+static const std::string MeclisOid{"MeclisOid"};
+static const std::string KararOid{"KararOid"};
+static const std::string Sayi{"Sayi"};
+}
+
+/**
+ * @brief The KararItem class Kararların Tutulduğu item
+ */
+class SERIKBLDCORE_EXPORT KararItem : public Item
+{
+public:
+    explicit KararItem();
+    explicit KararItem( const std::string& meclisOid );
+
+    KararItem& setMeclisOid( const std::string& meclisOid );
+    KararItem& setKararOid( const std::string& kararOid );
+    KararItem& setSayi( const std::int32_t& sayi );
+
+    std::int32_t sayi() const;
+    std::string kararOid() const;
+    std::string meclisOid() const;
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ *Linke Ait Keyler
+ */
+namespace LinkKey {
+static const std::string Collection{"MeclisYouTubeLinks"};
+static const std::string MeclisOid{"MeclisOid"};
+static const std::string VideoID{"videoID"};
+
+}
+
+
+/**
+ * @brief The YouTubeLink class : Meclis Canlı Yayınlarının Tutulduğu Item
+ */
+class SERIKBLDCORE_EXPORT YouTubeLink : public Item
+{
+public:
+    explicit YouTubeLink();
+
+    YouTubeLink& setVideoID( const std::string& videoid );
+    YouTubeLink& setMeclisOid( const std::string& meclisoid );
+
+    std::string videoid() const;
+    std::string videoUrl() const;
+    std::string videothump0() const;
+    std::string videothump1() const;
+    std::string videothump2() const;
+    std::string videothump3() const;
+
+    std::string meclisoid() const;
+};
+
+
+
+}
+
+
+
+
+
+}
+
 
 #endif // MECLISITEM_H
