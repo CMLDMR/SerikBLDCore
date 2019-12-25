4cd9c10196f2bf34543c13296f0bb30693184308
Cemal DEMİR <cemaldemir07@gmail.com>
2019-12-25 17:38:37 +0300

    Birçok Manager Eklendi.

--- meclis/meclismanager.cpp
+++ meclis/meclismanager.cpp
@@ -33,3 +33,70 @@ void SerikBLDCore::Meclis::YouTubeManager::onList(const QVector<SerikBLDCore::Me
 {
 
 }
+
+SerikBLDCore::Meclis::RaporManager::RaporManager(SerikBLDCore::DB *db)
+    : SerikBLDCore::ListItem<SerikBLDCore::Meclis::RaporItem> (db)
+{
+
+}
+
+void SerikBLDCore::Meclis::RaporManager::onList(const QVector<SerikBLDCore::Meclis::RaporItem> *mlist)
+{
+
+}
+
+SerikBLDCore::Meclis::TeklifManager::TeklifManager(SerikBLDCore::DB *db)
+    : SerikBLDCore::ListItem<SerikBLDCore::Meclis::TeklifItem> (db)
+{
+
+}
+
+void SerikBLDCore::Meclis::TeklifManager::onList(const QVector<SerikBLDCore::Meclis::TeklifItem> *mlist)
+{
+
+}
+
+SerikBLDCore::Meclis::UyeManager::UyeManager(SerikBLDCore::DB *db)
+    : SerikBLDCore::ListItem<SerikBLDCore::Meclis::MeclisUyesi> (db)
+{
+
+}
+
+void SerikBLDCore::Meclis::UyeManager::onList(const QVector<SerikBLDCore::Meclis::MeclisUyesi> *mlist)
+{
+
+}
+
+SerikBLDCore::Meclis::DonemManager::DonemManager(SerikBLDCore::DB *db)
+: SerikBLDCore::ListItem<SerikBLDCore::Meclis::MeclisDonemi> (db)
+{
+
+}
+
+void SerikBLDCore::Meclis::DonemManager::onList(const QVector<SerikBLDCore::Meclis::MeclisDonemi> *mlist)
+{
+
+}
+
+SerikBLDCore::Meclis::PartiManager::PartiManager(SerikBLDCore::DB *db)
+: SerikBLDCore::ListItem<SerikBLDCore::Meclis::PartiItem> (db)
+{
+
+}
+
+void SerikBLDCore::Meclis::PartiManager::onList(const QVector<SerikBLDCore::Meclis::PartiItem> *mlist)
+
+{
+
+}
+
+SerikBLDCore::Meclis::KomisyonManager::KomisyonManager(SerikBLDCore::DB *db)
+: SerikBLDCore::ListItem<SerikBLDCore::Meclis::KomisyonItem> (db)
+{
+
+}
+
+void SerikBLDCore::Meclis::KomisyonManager::onList(const QVector<SerikBLDCore::Meclis::KomisyonItem> *mlist)
+{
+
+}
--- meclis/meclismanager.h
+++ meclis/meclismanager.h
@@ -3,6 +3,7 @@
 
 #include "listitem.h"
 #include "meclisitem.h"
+#include "meclisuyesi.h"
 
 namespace SerikBLDCore {
 
@@ -46,6 +47,76 @@ public:
     virtual void onList(const QVector<YouTubeLink> *mlist) override;
 
 };
+
+
+
+class SERIKBLDCORE_EXPORT RaporManager : public SerikBLDCore::ListItem<SerikBLDCore::Meclis::RaporItem>
+{
+public:
+    explicit RaporManager(DB* db);
+
+
+    virtual void onList(const QVector<RaporItem> *mlist) override;
+
+};
+
+
+class SERIKBLDCORE_EXPORT TeklifManager : public SerikBLDCore::ListItem<SerikBLDCore::Meclis::TeklifItem>
+{
+public:
+    explicit TeklifManager(DB* db);
+
+    virtual void onList(const QVector<TeklifItem> *mlist) override;
+
+};
+
+
+
+
+class SERIKBLDCORE_EXPORT UyeManager : public SerikBLDCore::ListItem<SerikBLDCore::Meclis::MeclisUyesi>
+{
+public:
+    explicit UyeManager(DB* db);
+
+    virtual void onList(const QVector<MeclisUyesi> *mlist) override;
+
+};
+
+
+class SERIKBLDCORE_EXPORT DonemManager : public SerikBLDCore::ListItem<SerikBLDCore::Meclis::MeclisDonemi>
+{
+public:
+    explicit DonemManager(DB* db);
+
+    virtual void onList(const QVector<MeclisDonemi> *mlist) override;
+
+};
+
+
+class SERIKBLDCORE_EXPORT PartiManager : public SerikBLDCore::ListItem<SerikBLDCore::Meclis::PartiItem>
+{
+public:
+    explicit PartiManager(DB* db);
+
+    virtual void onList(const QVector<PartiItem> *mlist) override;
+
+};
+
+
+class SERIKBLDCORE_EXPORT KomisyonManager : public SerikBLDCore::ListItem<SerikBLDCore::Meclis::KomisyonItem>
+{
+public:
+    explicit KomisyonManager(DB* db);
+
+    virtual void onList(const QVector<KomisyonItem> *mlist) override;
+
+};
+
+
+
+
+
+
 
 
 }
