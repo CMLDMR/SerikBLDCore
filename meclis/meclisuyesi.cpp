210c7367a32a4991faa21b124b73e12e7de54d39
Cemal DEMİR <cemaldemir07@gmail.com>
2019-12-25 17:37:29 +0300

    Kısmı Tamamlandı.

--- meclis/meclisuyesi.cpp
+++ meclis/meclisuyesi.cpp
@@ -1,6 +1,141 @@
 #include "meclisuyesi.h"
 
-MeclisUyesi::MeclisUyesi()
+
+SerikBLDCore::Meclis::MeclisUyesi::MeclisUyesi()
+    :SerikBLDCore::TC (UyeKey::Collection)
 {
 
+}
+
+SerikBLDCore::Meclis::MeclisUyesi &SerikBLDCore::Meclis::MeclisUyesi::setTCOid(const std::string &tcOid)
+{
+    this->append(UyeKey::tcOid,bsoncxx::oid{tcOid});
+    return *this;
+}
+
+SerikBLDCore::Meclis::MeclisUyesi &SerikBLDCore::Meclis::MeclisUyesi::setTCOid(const bsoncxx::oid &tcOid)
+{
+    this->append(UyeKey::tcOid,tcOid);
+    return *this;
+}
+
+SerikBLDCore::Meclis::MeclisUyesi &SerikBLDCore::Meclis::MeclisUyesi::setPartiOid(const std::string &partiOid)
+{
+    this->append(UyeKey::partiOid,bsoncxx::oid{partiOid});
+    return *this;
+}
+
+SerikBLDCore::Meclis::MeclisUyesi &SerikBLDCore::Meclis::MeclisUyesi::setPartiOid(const bsoncxx::oid &partiOid)
+{
+    this->append(UyeKey::partiOid,partiOid);
+    return *this;
+}
+
+SerikBLDCore::Meclis::MeclisUyesi &SerikBLDCore::Meclis::MeclisUyesi::setKomisyonOid(const std::string &komisyonOid)
+{
+    this->append(UyeKey::komisyonOid,bsoncxx::oid{komisyonOid});
+    return *this;
+}
+
+SerikBLDCore::Meclis::MeclisUyesi &SerikBLDCore::Meclis::MeclisUyesi::setKomisyonOid(const bsoncxx::oid &komisyonOid)
+{
+    this->append(UyeKey::komisyonOid,komisyonOid);
+    return *this;
+}
+
+QString SerikBLDCore::Meclis::MeclisUyesi::tcOid() const
+{
+    auto val = this->element (UyeKey::tcOid);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_oid ().value.to_string ());
+    }
+    return "";
+}
+
+QString SerikBLDCore::Meclis::MeclisUyesi::partiOid() const
+{
+    auto val = this->element (UyeKey::partiOid);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_oid ().value.to_string ());
+    }
+    return "";
+}
+
+QString SerikBLDCore::Meclis::MeclisUyesi::komisyonOid() const
+{
+    auto val = this->element (UyeKey::komisyonOid);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_oid ().value.to_string ());
+    }
+    return "";
+}
+
+
+SerikBLDCore::Meclis::MeclisDonemi::MeclisDonemi()
+    :SerikBLDCore::Item (DonemKey::Collection)
+{
+
+}
+
+SerikBLDCore::Meclis::MeclisDonemi &SerikBLDCore::Meclis::MeclisDonemi::setDonem(const std::string &donem)
+{
+    this->append(DonemKey::donem,donem);
+    return *this;
+}
+
+QString SerikBLDCore::Meclis::MeclisDonemi::donem() const
+{
+    auto val = this->element (DonemKey::donem);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_utf8 ().value.to_string ());
+    }
+    return "";
+}
+
+SerikBLDCore::Meclis::PartiItem::PartiItem()
+    :SerikBLDCore::Item (PartiKey::Collection)
+{
+
+}
+
+SerikBLDCore::Meclis::PartiItem &SerikBLDCore::Meclis::PartiItem::setParti(const std::string &parti)
+{
+    this->append(PartiKey::parti,parti);
+    return *this;
+}
+
+QString SerikBLDCore::Meclis::PartiItem::parti() const
+{
+    auto val = this->element (PartiKey::parti);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_utf8 ().value.to_string ());
+    }
+    return "";
+}
+
+SerikBLDCore::Meclis::KomisyonItem::KomisyonItem()
+    :SerikBLDCore::Item (KomisyonKey::Collection)
+{
+
+}
+
+SerikBLDCore::Meclis::KomisyonItem &SerikBLDCore::Meclis::KomisyonItem::setKomisyonAdi(const std::string &komisyonAdi)
+{
+    this->append(KomisyonKey::komisyon,komisyonAdi);
+    return *this;
+}
+
+QString SerikBLDCore::Meclis::KomisyonItem::komisyonAdi() const
+{
+    auto val = this->element (KomisyonKey::komisyon);
+    if( val )
+    {
+        return QString::fromStdString (val.value ().get_utf8 ().value.to_string ());
+    }
+    return "";
 }
--- meclis/meclisuyesi.h
+++ meclis/meclisuyesi.h
@@ -1,11 +1,97 @@
 #ifndef MECLISUYESI_H
 #define MECLISUYESI_H
 
+#include "tc.h"
 
-class MeclisUyesi : public Item
+
+namespace SerikBLDCore {
+
+
+namespace Meclis {
+
+
+namespace UyeKey {
+static const std::string Collection{"MeclisUyeler"};
+static const std::string tcOid{"tcOid"};
+static const std::string partiOid{"partiOid"};
+static const std::string komisyonOid{"komisyonOid"};
+}
+
+
+
+class SERIKBLDCORE_EXPORT MeclisUyesi : public SerikBLDCore::TC
 {
 public:
     MeclisUyesi();
+    MeclisUyesi& setTCOid( const std::string& tcOid );
+    MeclisUyesi& setTCOid( const bsoncxx::oid& tcOid );
+    MeclisUyesi& setPartiOid( const std::string& partiOid );
+    MeclisUyesi& setPartiOid( const bsoncxx::oid& partiOid );
+    MeclisUyesi& setKomisyonOid( const std::string& komisyonOid );
+    MeclisUyesi& setKomisyonOid( const bsoncxx::oid& komisyonOid );
+    QString tcOid() const;
+    QString partiOid() const;
+    QString komisyonOid() const;
 };
+
+
+namespace DonemKey {
+static const std::string Collection{"MeclisDonemi"};
+static const std::string donem{"donem"};
+}
+
+class SERIKBLDCORE_EXPORT MeclisDonemi : public SerikBLDCore::Item
+{
+public:
+    explicit MeclisDonemi();
+
+    MeclisDonemi& setDonem( const std::string& donem );
+
+    QString donem() const;
+};
+
+
+namespace PartiKey {
+static const std::string Collection{"Partiler"};
+static const std::string parti{"parti"};
+}
+
+class SERIKBLDCORE_EXPORT PartiItem : public SerikBLDCore::Item
+{
+public:
+    explicit PartiItem();
+
+    PartiItem& setParti( const std::string& parti );
+
+    QString parti() const;
+};
+
+
+namespace KomisyonKey {
+static const std::string Collection{"Komisyonlar"};
+static const std::string komisyon{"komisyon"};
+}
+
+class SERIKBLDCORE_EXPORT KomisyonItem : public SerikBLDCore::Item
+{
+public:
+    explicit KomisyonItem();
+
+    KomisyonItem& setKomisyonAdi( const std::string& komisyonAdi );
+
+    QString komisyonAdi() const;
+};
+
+
+
+
+}
+
+
+
+}
+
+
+
 
 #endif // MECLISUYESI_H
