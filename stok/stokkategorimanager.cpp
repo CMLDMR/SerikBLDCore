8b132f90e19c2baa856f6cf55e50ee0bb84a0c7f
Cemal DEMİR <cemaldemir07@gmail.com>
2020-01-21 17:36:08 +0300

    StokManager Tamamlandı.

--- stok/stokkategorimanager.cpp
+++ stok/stokkategorimanager.cpp
@@ -32,3 +32,91 @@ void SerikBLDCore::Stok::StokKodManager::errorOccured(const std::string &errorTe
 {
 
 }
+
+SerikBLDCore::Stok::StokManager::StokManager(SerikBLDCore::User *_mUser)
+    :SerikBLDCore::ListItem<SerikBLDCore::Stok::Stok> (_mUser->getDB ()),
+      SerikBLDCore::Stok::StokKodManager (_mUser->getDB ()),
+      SerikBLDCore::Stok::StokKategoriManager (_mUser->getDB ()),
+      mUser(_mUser)
+{
+
+}
+
+std::string SerikBLDCore::Stok::StokManager::createStok(SerikBLDCore::Stok::Stok &stok)
+{
+    auto countItem = SerikBLDCore::ListItem<SerikBLDCore::Stok::Stok>::countItem (stok);
+    if( countItem )
+    {
+        return "";
+    }
+    return SerikBLDCore::ListItem<SerikBLDCore::Stok::Stok>::InsertItem (stok);
+}
+
+void SerikBLDCore::Stok::StokManager::onList(const QVector<SerikBLDCore::Stok::Stok> *mlist)
+{
+
+}
+
+void SerikBLDCore::Stok::StokManager::errorOccured(const std::string &errorText)
+{
+
+}
+
+bool SerikBLDCore::Stok::StokManager::addStok(const SerikBLDCore::Stok::Stok &stok)
+{
+    if( stok.getCollection () != StokKey::Collection )
+    {
+        SerikBLDCore::ListItem<SerikBLDCore::Stok::Stok>::setLastError ("Stok Item Giriş Collection Değil");
+        return false;
+    }
+    if( SerikBLDCore::ListItem<SerikBLDCore::Stok::Stok>::InsertItem (stok).size () )
+    {
+        return true;
+    }
+    return false;
+}
+
+bool SerikBLDCore::Stok::StokManager::reduceStok(const SerikBLDCore::Stok::Stok &stok, const int64_t &miktar)
+{
+    if( stok.getCollection () != StokKey::CollectionCikis )
+    {
+        SerikBLDCore::ListItem<SerikBLDCore::Stok::Stok>::setLastError ("Stok Item Çıkış Collection Değil");
+        return false;
+    }
+    if( SerikBLDCore::ListItem<SerikBLDCore::Stok::Stok>::InsertItem (stok).size () )
+    {
+        return true;
+    }
+    return false;
+}
+
+void SerikBLDCore::Stok::StokManager::onList(const QVector<SerikBLDCore::Stok::StokKodItem> *mlist)
+{
+
+}
+
+void SerikBLDCore::Stok::StokManager::updateStokList(const SerikBLDCore::Stok::Stok filter)
+{
+    SerikBLDCore::ListItem<SerikBLDCore::Stok::Stok>::UpdateList (filter);
+}
+
+void SerikBLDCore::Stok::StokManager::updateStokKodList(const SerikBLDCore::Stok::StokKodItem filter)
+{
+    SerikBLDCore::Stok::StokKodManager::UpdateList (filter);
+}
+
+void SerikBLDCore::Stok::StokManager::updateKategoriList(const SerikBLDCore::Stok::StokKategori filter)
+{
+    SerikBLDCore::Stok::StokKategoriManager::UpdateList (filter);
+
+}
+
+void SerikBLDCore::Stok::StokManager::onList(const QVector<SerikBLDCore::Stok::StokKategori> *mlist)
+{
+
+}
+
+SerikBLDCore::User *SerikBLDCore::Stok::StokManager::user() const
+{
+    return mUser;
+}
--- stok/stokkategorimanager.h
+++ stok/stokkategorimanager.h
@@ -3,6 +3,7 @@
 
 #include "stok.h"
 #include "listitem.h"
+#include "user.h"
 
 namespace SerikBLDCore {
 
@@ -28,6 +29,51 @@ public:
 
     virtual void errorOccured(const std::string &errorText) override;
 };
+
+
+class SERIKBLDCORE_EXPORT StokManager :
+        public SerikBLDCore::ListItem<SerikBLDCore::Stok::Stok> ,
+        public SerikBLDCore::Stok::StokKodManager,
+        public SerikBLDCore::Stok::StokKategoriManager
+{
+public:
+    explicit StokManager( SerikBLDCore::User* _mUser );
+
+
+    /**
+     * @brief createStok: 0 miktarında Stok Oluşturur.
+     * @param stok
+     * @return Stok varsa false eğer stok yok ise true döndürür.
+     */
+    virtual std::string createStok( SerikBLDCore::Stok::Stok& stok );
+
+    virtual void onList(const QVector<Stok> *mlist) override;
+
+    virtual void errorOccured(const std::string &errorText) override;
+
+    virtual bool addStok(const SerikBLDCore::Stok::Stok& stok );
+
+    virtual bool reduceStok(const SerikBLDCore::Stok::Stok& stok , const std::int64_t& miktar );
+
+    virtual void onList(const QVector<StokKodItem> *mlist) override;
+
+    void updateStokList( const SerikBLDCore::Stok::Stok filter = SerikBLDCore::Stok::Stok() );
+
+    void updateStokKodList( const SerikBLDCore::Stok::StokKodItem filter = SerikBLDCore::Stok::StokKodItem() );
+
+    void updateKategoriList( const SerikBLDCore::Stok::StokKategori filter = SerikBLDCore::Stok::StokKategori() );
+
+
+    virtual void onList(const QVector<SerikBLDCore::Stok::StokKategori> *mlist) override;
+
+
+
+    SerikBLDCore::User* user() const;
+
+private:
+    SerikBLDCore::User* mUser;
+};
+
 
 
 }
