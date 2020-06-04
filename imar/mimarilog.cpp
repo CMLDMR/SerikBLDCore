3cc14d61398ba5c7110263c82ff8ab7e82fd4fb1
Cemal DEMİR <cemaldemir07@gmail.com>
2020-06-04 10:24:37 +0300

    MimariLog Eklendi.

---
+++ imar/mimarilog.cpp
@@ -0,0 +1,6 @@
+#include "mimarilog.h"
+
+MimariLog::MimariLog()
+{
+
+}
---
+++ imar/mimarilog.h
@@ -0,0 +1,11 @@
+#ifndef MIMARILOG_H
+#define MIMARILOG_H
+
+
+class MimariLog : public SerikBLDCore::Item
+{
+public:
+    MimariLog();
+};
+
+#endif // MIMARILOG_H
